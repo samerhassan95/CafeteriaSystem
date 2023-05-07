73453b2f0e761689340e46652dd7472c629ccfe6
samerhassan95 <arch.samerhassan@gmail.com>
2023-05-07 23:38:49 +0300

    merge

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DB_HOST", "localhost:3906");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_DATABASE", "php_cafe");        
+define("DB_HOST", "localhost:3306");
+define("DB_USER", "samer");
+define("DB_PASS", "samerhassan11");
+define("DB_DATABASE", "samer");
 return new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASS);
